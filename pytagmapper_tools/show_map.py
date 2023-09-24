8f34ffb45683df0fff188574a48e44ebda1d2da4
Bibidkun <maru.dai.23.25@gmail.com>
2023-09-24 22:45:42 +0900

    pass to mac

--- pytagmapper_tools/show_map.py
+++ pytagmapper_tools/show_map.py
@@ -13,10 +13,12 @@ from pytagmapper.map_builder import solvePnPWrapper
 def preparate_data():
     pass
 
-# def estimate_camera_view_points():
-#     tx_camera_world = solvePnPWrapper(tags_world_coords, tags_img_coords, _camera_matrix, _distortion)
-#     camera_view_point = SE3_inv(tx_camera_world)
-#     return camera_view_point
+def estimate_camera_view_points(tags_world_coords, tags_img_coords, cam_params):
+    _camera_matrix = cam_params['camera_matrix']
+    _distortion = cam_params['distortion_coefficients']
+    tx_camera_world = solvePnPWrapper(tags_world_coords, tags_img_coords, _camera_matrix, _distortion)
+    camera_view_point = SE3_inv(tx_camera_world)
+    return camera_view_point
 
 def main():
 
@@ -82,7 +84,7 @@ def main():
                 y2 = world_corners[1,(i+1)%4]
                 line = plt.Line2D((x1,x2), (y1,y2), lw=1.5)
                 plt.gca().add_line(line)
-            print(world_corners.copy()[:2,:].T)
+            # print(world_corners.copy()[:2,:].T)
             tag_corners_world[tag_id] = world_corners.copy()[:2,:].T # 3dの座標
 
             center = np.sum(world_corners, axis=1)/4
@@ -100,9 +102,13 @@ def main():
         raise RuntimeError("Unsupported map type", map_type)
     
     tags_world_coords = np.vstack(tag_corners_world.values()) # id順に並べた3dの座標
-    for cam_point, mat_corners in scene_data['viewpoints'].values():
-        mat_corners = OrderedDict(sorted(mat_corners.items()))
-        corners_2d = np.vstack(cam_point.values())
+    for cam_view in scene_data['viewpoints'].values():
+        cam_id, tags = cam_view["cam_id"], OrderedDict(sorted(cam_view["tags"].items()))
+        array = np.vstack([np.array(tag_corner).reshape(4,2) for tag_corner in tags.values()])
+        
+        camera_world_coords  = estimate_camera_view_points(tags_world_coords, np.vstack(tags.values()), scene_data['camera_matrix'][int(cam_id)])
+        print(f"cam_id: {cam_id}")
+        print(camera_world_coords)
         
     plt.axis('scaled')
     plt.show()
